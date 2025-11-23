09eaf2d2332e443f74c61a738c256deec123b3a2
zihua0506-glitch <zihua0506@berkeley.edu>
2025-11-22 16:39:58 -0800

    requirement update

---
+++ service/social_data_source.py
@@ -0,0 +1,5 @@
+def get_social_data_source():
+    # grab the social data from the social media platforms like twitter, reddit, discord, youtube, etc.
+    # the social data(in crypto) keyword frequency analysis, Sentiment polarity, Topic popularity
+    # return the social data
+    pass
