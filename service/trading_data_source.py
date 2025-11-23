695de711b9038dac086b7eb1155506bb44fd5687
zihua0506-glitch <zihua0506@berkeley.edu>
2025-11-22 16:39:58 -0800

    requirement update

---
+++ service/trading_data_source.py
@@ -0,0 +1,5 @@
+def get_trading_data_source():
+    # grab the trading data from the trading platforms like binance, okex, etc.
+    # the trading data includes the price of the crypto, the volume of transactions, etc.
+    # return the trading data
+    pass
