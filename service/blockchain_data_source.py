89a640ebdef68b6b42b1e500dfcd2a5851930c56
zihua0506-glitch <zihua0506@berkeley.edu>
2025-11-22 22:56:17 -0800

    update

--- service/blockchain_data_source.py
+++
@@ -1,6 +0,0 @@
-def get_blockchain_data_source():
-    # grab the blockchain data from the blockchain (web3.0 extension)
-    #we just grab the 5 most popular cryptos(like bitcoin, ethereum, sol,ripple, etc.)
-    # the blockchain data includes the volume of transactions, the wallet activity, number of blocks, number of miners, etc.
-    # return the blockchain data
-    pass
--- service/trading_data_source.py
+++
@@ -1,5 +0,0 @@
-def get_trading_data_source():
-    # grab the trading data from the trading platforms like binance, okex, etc.
-    # the trading data includes the price of the crypto, the volume of transactions, etc.
-    # return the trading data
-    pass
